7317941fcdba8022f9d5e2d352208b38d0073ef5
rafaelino1707 <up202406634@up.pt>
2025-12-11 10:16:18 +0000

    Update main.cpp

--- ESP32-S3 Dev Module/src/main.cpp
+++ ESP32-S3 Dev Module/src/main.cpp
@@ -2,8 +2,8 @@
 #include "pulsecounting.h"
 
 #define SENSOR_GPIO     38        // NE555 Out Connect
-#define SAMPLE_TIME_MS  100       // Window of 0.5s
-#define N_WINDOWS_AVG   8         // Number of Average Windows
+#define SAMPLE_TIME_MS  200      // 0.2 s por janela
+#define N_WINDOWS_AVG   10       // 10 janelas -> 2.0 s efetivos
 
 void setup()
 {
@@ -22,13 +22,13 @@ void loop()
     static uint32_t t0_ms = millis();
 
     // Average Frequency with Outliers Rejection
-    float freq_Hz = pcnt_measure_frequency_avg(SAMPLE_TIME_MS, N_WINDOWS_AVG);
+    float freq_Hz = pcnt_measure_frequency_avg(SAMPLE_TIME_MS, N_WINDOWS_AVG)/0.86;
 
     uint32_t now_ms = millis();
     uint32_t elapsed_ms = now_ms - t0_ms;
 
     // Stimating Capacitance
-    double C_est_F = pcnt_freq_to_capacitance_F(freq_Hz, 3.9e6, 4.7e6);
+    double C_est_F = pcnt_freq_to_capacitance_F(freq_Hz, 0.5e6, 1e6);
 
     // CSV: sample_index,elapsed_ms,freq_Hz,C_est_F
     Serial.print(sampleIndex);
