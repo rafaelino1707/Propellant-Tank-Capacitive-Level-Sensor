b4c50f01b07add1fe64f0817d099576bf5188025
rafaelino1707 <up202406634@up.pt>
2025-12-11 14:52:07 +0000

    Working

--- ESP32-S3 Dev Module/plotting/pila.py
+++ ESP32-S3 Dev Module/plotting/pila.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 
-filename = "log/dados_20251206_020303.csv"
+filename = "log/aaaapF.csv"
 
 time_s = []
 freq = []
+cmes=[]
 
 with open(filename, "r", encoding="utf-8") as f:
     header = f.readline()  # lê cabeçalho e ignora
@@ -16,31 +17,45 @@ with open(filename, "r", encoding="utf-8") as f:
 
         parts = line.split(",")
 
-        if len(parts) != 4:
+        if len(parts) != 5:
             print("Linha inválida:", line)
             continue
 
         try:
             elapsed_ms = float(parts[1])
-            freq_Hz    = float(parts[3])
+            freq_Hz    = float(parts[2])
+            cmeas = float(parts[4])
         except ValueError:
             print("Erro a converter linha:", line)
             continue
 
         time_s.append(elapsed_ms / 1000.0)
         freq.append(freq_Hz)
+        cmes.append(cmeas*1e12)
 
 # Debug: ver listas
 print("time_s:", time_s)
 print("freq:", freq)
+print(cmes)
 
 # Plot
 plt.figure(figsize=(8,4))
 plt.plot(time_s, freq, color="r")
 plt.xlabel("Time [s]")
 plt.ylabel("Frequency [Hz]")
-plt.ylim(0, 1500)
+plt.ylim(0, 5000)
 plt.title("Frequency [Hz] vs Time [s]")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+
+plt.figure(figsize=(8,4))
+plt.plot(time_s, cmes, color="g")
+plt.xlabel("Time [s]")
+plt.ylabel("Capacitance [pF]")
+plt.ylim(0, 200)
+plt.title("Capacitance [pF] vs Time [s]")
 plt.grid(True)
 plt.tight_layout()
 plt.show()
